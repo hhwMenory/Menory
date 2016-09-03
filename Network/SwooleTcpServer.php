098eaf99f02628384f9c67f1cd9a351f608413b4
hhw <hhw_menory@yeah.net>
2016-09-03 23:42:42 +0800

    create /Network/SwooleTcpServer.php

---
+++ Network/SwooleTcpServer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @todo 
+ * @author Menory
+ * @date 2016年9月3日 21:04:11
+ */
+namespace Menory\Network;
+// use \Menory\Network\SwooleBaseServer;
+
+class SwooleTcpServer // extends SwooleBaseServer
+{
+	public function run()
+	{
+		echo 'running...';
+	}
+}
